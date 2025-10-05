bfc30d94c9e0dc196903b33f93203957ce03228a
Sushant Negi <121372413+CARBANOID@users.noreply.github.com>
2025-10-05 11:33:48 +0530

    Remove commented server configuration from vite.config.ts

--- DigiBrain/frontend/vite.config.ts
+++ DigiBrain/frontend/vite.config.ts
@@ -4,9 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss()],
-  // server: {
-  //   host: '10.160.112.39', // listen on all addresses
-  //   port: 5173,      // optional, default is 5173
-  // },
+  plugins: [react() , tailwindcss()]
 })
